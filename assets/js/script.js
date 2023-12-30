fd0cae4859374b6d7c1dfe1f28f5a07fa38e1ad0
Godson Saji <Godsonsaji832@gmail.com>
2023-12-30 21:12:53 +0530

    Updated JS File

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,18 @@ const navbar = document.querySelector("[data-navbar]");
 const elemToggleFunc = function (elem) {elem.classList.toggle("active"); }
 
 menuToggleBtn.addEventListener("click", function () {elemToggleFunc(navbar);});
+
+/**
+ * go to top
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+    if(window.scrollY >= 800) {
+        goTopBtn.classList.add("active");
+    }else{
+        goTopBtn.classList.remove("active");
+    }
+});
